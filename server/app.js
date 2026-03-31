f9f88369bb6a6f1c5ad9bbb33ab773704f004265
paksak4 <paksak4@naver.com>
2026-03-31 14:34:35 +0900

    Init: BE 폴더 구조 세팅

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "mymvc02 backend server running" });
+});
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT} 에서 서버 실행 중`);
+})
