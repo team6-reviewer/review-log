aa5f2bb5c7001682a9ba7a1a0fabc4673dd0114c
zer0p01nt <1027.bmy@gmail.com>
2026-04-01 18:22:47 +0900

    Chore: 페이지 컴포넌트 생성 및 라우팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,26 @@
+import Error from "@/pages/error/Error";
+import Home from "@/pages/home/Home";
+import Join from "@/pages/join/Join";
+import Login from "@/pages/login/Login";
+import Mypage from "@/pages/mypage/Mypage";
+import Withdraw from "@/pages/withdraw/Withdraw";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div>Review-Log</div>;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/join' element={<Join />} />
+      </Routes>
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/mypage' element={<Mypage />} />
+        <Route path='/withdraw' element={<Withdraw />} />
+      </Routes>
+      <Route path='*' element={<Error />} />
+    </>
+  );
 }
 
 export default App;
