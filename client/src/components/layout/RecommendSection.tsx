aa01d81eaeb52bca02f14fda9e7527196cbc9121
zer0p01nt <1027.bmy@gmail.com>
2026-04-06 15:05:19 +0900

    Design: 태그 기반 추천 섹션 타이틀 수정 및 설명 추가

--- client/src/components/layout/RecommendSection.tsx
+++ client/src/components/layout/RecommendSection.tsx
@@ -25,11 +25,14 @@ export default function RecommendSection() {
 
   return (
     <section className='shadow-sm bg-tag-recomm px-8 py-6 rounded-lg flex flex-col lg:flex-row items-center justify-between gap-8 relative overflow-hidden'>
-      <div className='flex flex-col gap-4 z-10 shrink-0 min-w-fit select-none'>
-        <h2 className='text-[clamp(20px,3vw,32px)] font-bold leading-tight whitespace-nowrap text-main-gray'>
-          <span>당신의 취향을 저격할 </span>
-          <span>추천 리뷰</span>
-        </h2>
+      <div className='flex flex-col items-center lg:items-start gap-2'>
+        <div className='flex flex-row gap-2 lg:flex-col lg:gap-0 z-10 shrink-0 min-w-fit select-none text-[clamp(20px,3vw,32px)] font-bold leading-tight whitespace-nowrap text-main-gray'>
+          <span>당신도 좋아할 </span>
+          <span>다른 작품 추천 리뷰</span>
+        </div>
+        <span className='text-main-gray text-sm break-keep'>
+          당신의 마음에 든 작품의 태그를 바탕으로 선정했어요.
+        </span>
       </div>
 
       {/* 드래그 되는 동안 마우스 커서도 구분 */}
