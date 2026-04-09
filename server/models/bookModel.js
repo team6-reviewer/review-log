71a63dea4ad26cb32a1e62808d0b9eee1638f574
paksak4 <paksak4@naver.com>
2026-04-09 10:04:37 +0900

    Refactor: api파트 코드 주석 작성 및 알라딘 api서 book cover 누락 방지 위한 예외처리

--- server/models/bookModel.js
+++ server/models/bookModel.js
@@ -3,23 +3,30 @@ const aladinBaseUrl = 'http://www.aladin.co.kr/ttb/api';
 
 // 알라딘 도서 판매량 순위(베스트셀러) 조회 (상위 5개)
 exports.getTopBooks = async () => {
-  const ttbKey = process.env.ALADIN_TTB_KEY; // 알라딘에서 발급받은 TTB Key
+  const ttbKey = process.env.ALADIN_TTB_KEY; // 알라딘에서 발급받은 TTB Key (api 인증키)
   
-  // QueryType=Bestseller (판매량 순위), SearchTarget=Book(국내도서), output=js 즉 알라딘 사이트에서의 주간 베스트 셀러라고 보면 됨
+  // QueryType=Bestseller (판매량 순위), MaxResults=5(최대결과 5개), SearchTarget=Book(국내도서), output=js 이고,
+  // 이는 알라딘 사이트에서의 주간 베스트 셀러 도서 상위 5개를 가져온거라 보면 됨
   const topUrl = `${aladinBaseUrl}/ItemList.aspx?ttbkey=${ttbKey}&QueryType=Bestseller&MaxResults=5&start=1&SearchTarget=Book&output=js&Version=20131101`;
 
   try {
+    // fetch 메소드를 사용하여 알라딘 api 에 요청보내고 response 받아옴
     const response = await fetch(topUrl);
+    // 만약 응답이 정상적이지 않으면 에러 throw
     if (!response.ok) {
       throw new Error(`HTTP error ${response.status}`);
     }
+    // json 응답 데이터 빼오기
     const data = await response.json();
     
+    // 가져온 결과 중에서 알라딘 도서 고유 id, 제목, 포스터 경로만 정제하여 반환
     return data.item.map(book => ({
       id: book.itemId, // 알라딘 관리 도서 ID
       title: book.title, // 제목
-      posterPath: (book.cover).replace('/coversum/', '/cover500/') || null // 표지경로, 없으면 null
+      posterPath: book.cover ? book.cover.replace('/coversum/', '/cover500/') : null // 표지경로, 없으면 null
+      // 저해상도의 표지 이미지를 고해상도 500px로 바꿈
     }));
+
   } catch (error) {
     console.error('알라딘 판매량 순위 API 호출 에러:', error);
     throw error;
@@ -29,20 +36,29 @@ exports.getTopBooks = async () => {
 
 // 알라딘 API를 통해 도서 제목으로 검색하는 모델
 exports.searchBookByTitle = async (title) => {
+  // 알라딘 api 인증키
   const ttbKey = process.env.ALADIN_TTB_KEY;
+  // QueryType=Title 제목검색, MaxResults=10 결과 10개 반환, SearchTarget=Book 도서 검색
+  // 알라딘에서 제목으로 도서 검색해오는 url
   const searchUrl = `${aladinBaseUrl}/ItemSearch.aspx?ttbkey=${ttbKey}&Query=${encodeURIComponent(title)}&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101`;
 
   try {
+    // fetch 메소드를 사용하여 알라딘 api에 요청보내고 response 받아옴
     const response = await fetch(searchUrl);
+    // 만약 응답이 정상적이지 않으면 에러 throw
     if (!response.ok) {
       throw new Error(`HTTP error ${response.status}`);
     }
+
+    // json 응답 데이터
     const data = await response.json();
 
+    // 가져온 결과 중에서 알라딘 도서 고유 id, 제목, 포스터 경로만 정제하여 반환  
     return data.item.map(book => ({
       id: book.itemId, // 알라딘 관리 도서 ID
       title: book.title, // 제목
-      posterPath: (book.cover).replace('/coversum/', '/cover500/') || null // 표지경로, 없으면 null
+      posterPath: book.cover ? book.cover.replace('/coversum/', '/cover500/') : null // 표지경로, 없으면 null
+      // 저해상도의 표지 이미지를 고해상도 500px로 바꿈
     }));
   } catch (error) {
     console.error('알라딘 검색 API 호출 에러:', error);
--- server/models/movieModel.js
+++ server/models/movieModel.js
@@ -2,35 +2,41 @@
 const url = `http://api.themoviedb.org/3`;
 const posterUrl = `https://image.tmdb.org/t/p/w500`; // 포스터 경로 가져오기 위함
 
+// TMDB 기준 현재 상영작 상위 5작품 가져오기(상위의 기준은 TMDB 실시간 화제작 기준이라 함)
 exports.getTopMovies = async () => {
 
   const token = process.env.TMDB_ACCESS_TOKEN; // TMDB에서 발급받은 토큰
 
-  // 언어, 지역 한국으로하고 현재 상영작 1페이지 조회 (마지막에 추출할 때 5개만 추출)
+  // 언어를 한국으로하고 현재 상영작 1페이지 조회 (마지막에 추출할 때 5개만 추출)
   const topUrl = `${url}/movie/now_playing?language=ko-KR&region=KR&page=1`;
 
+  // tmdb api 인증키가 토큰 방식으로 구성되어 있으므로 
+  // http 헤더에 담음
   const options = {
     method: 'GET',
     headers: {
-      accept: 'application/json',
+      accept: 'application/json', // response json 형태로 받기
       Authorization: `Bearer ${token}` // Bearer 토큰 형식
     }
   };
 
   try {
-    
+    // fetch 를 사용하여 TMDB에 요청 보내고 응답을 얻어옴
     const response = await fetch(topUrl, options);
+
+    // 만약 응답이 정상적이지 않으면 에러 throw
     if (!response.ok) {
       throw new Error(`HTTP error${response.status}`);
     }
+
+    // json 응답 데이터 빼오기
     const data = await response.json();
-    
     // 가져온 결과 중 상위 5개 작품 추출 및 필요한 데이터(제목,포스터경로) 만 정제하여 반환
     const top5Movies = data.results.slice(0, 5);
     return top5Movies.map(movie => ({
       id: movie.id, // TMDB 관리 영화 ID
       title: movie.title, // 제목
-      posterPath: movie.poster_path ? `${posterUrl}${movie.poster_path}` : null
+      posterPath: movie.poster_path ? `${posterUrl}${movie.poster_path}` : null // 포스터 이미지 경로, 만약 없으면 null로 처리
     }));
   } catch (error) {
     console.error('TMDB API 호출 에러:', error);
@@ -40,31 +46,36 @@ exports.getTopMovies = async () => {
 
 // TMDB API를 통해 영화 제목으로 검색하는 모델
 exports.searchMovieByTitle = async (title) => {
+  // TBDB 토큰 (api 인증키)
   const token = process.env.TMDB_ACCESS_TOKEN;
   // 언어, 지역 한국으로하고 현재 title에 해당하는 작품 반환 
   const searchUrl = `${url}/search/movie?query=${encodeURIComponent(title)}&language=ko-KR`;
 
+  // 마찬가지로 토큰(인증키)를 헤더에 담기
   const options = {
     method: 'GET',
     headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${token}`
+      accept: 'application/json', // json 형식으로 데이터 반환 받겠다는 뜻
+      Authorization: `Bearer ${token}` // bearer 토큰 담고
     }
   };
 
   try {
+    // fetch 메소드 사용하여 TMDB에 요청보내고 response를 받아옴
     const response = await fetch(searchUrl, options);
+    // 만약 응답이 정상적이지 않으면 에러 throw
     if (!response.ok) {
       throw new Error(`HTTP error${response.status}`);
     }
 
+    // json 응답 데이터 빼오기
     const data = await response.json();
 
     // 마찬가지로 제목, 포스터 경로만 추출하여 반환
     return data.results.map(movie => ({
       id: movie.id, // TMDB 관리 영화 ID
-      title: movie.title,
-      posterPath: movie.poster_path ? `${posterUrl}${movie.poster_path}` : null
+      title: movie.title, // 영화 제목
+      posterPath: movie.poster_path ? `${posterUrl}${movie.poster_path}` : null // 포스터의 경로, 만약 없으면 null
     }));
 
   } catch (error) {
